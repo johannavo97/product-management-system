3e67d171d851cc294942080487dca73c96e9c9e9
Johanna Vo <thanhtruc2009@gmail.com>
2022-06-21 14:00:01 -0700

    First Commit

---
+++ src/main/java/com/student/ordermanagementproject/OrdermanagementprojectApplication.java
@@ -0,0 +1,13 @@
+package com.student.ordermanagementproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermanagementprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermanagementprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/student/ordermanagementproject/OrdermanagementprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.ordermanagementproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermanagementprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
