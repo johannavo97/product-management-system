ee50f23db113cc6ff6f28eccfcf5fc27ab9387ec
Johanna Vo <thanhtruc2009@gmail.com>
2022-06-27 17:07:32 -0700

    update models

---
+++ src/main/java/com/student/ordermanagementproject/models/Product.java
@@ -0,0 +1,60 @@
+package com.student.ordermanagementproject.models;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Slf4j
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "product")
+@Entity
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int productId;
+    @NonNull
+    String productName;
+    @NonNull
+    String productCategory;
+    @NonNull
+    Double productPrice;
+
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "products", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.EAGER)
+    Set<User> users = new LinkedHashSet<>();
+
+    public void addUser(User user){
+        users.add(user);
+        user.getProducts().add(this);
+    }
+
+    public Product(int productId, @NonNull String productName, @NonNull String productCategory, @NonNull Double productPrice) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productCategory = productCategory;
+        this.productPrice = productPrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productId == product.productId && productName.equals(product.productName) && productCategory.equals(product.productCategory) && productPrice == product.productPrice;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId,productName, productCategory, productPrice);
+
+    }
+}
---
+++ src/main/java/com/student/ordermanagementproject/models/User.java
@@ -0,0 +1,67 @@
+package com.student.ordermanagementproject.models;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Slf4j
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "user")
+@Entity
+public class User {
+
+    @Id @NonNull
+    String email;
+
+    @NonNull
+    String name;
+
+    @NonNull
+    @Setter(AccessLevel.NONE)
+    String password;
+
+    public User(@NonNull String email, @NonNull String name, @NonNull String password) {
+        this.email = email;
+        this.name = name;
+        this.password =  new BCryptPasswordEncoder().encode(password);
+    }
+
+    public void setPassword(String password) {
+        this.password = new BCryptPasswordEncoder().encode(password);
+    }
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_products",
+            joinColumns = @JoinColumn(name = "user_email"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
+    Set<Product> products = new LinkedHashSet<>();
+
+    public void addProduct(Product product){
+        products.add(product);
+        product.getUsers().add(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return email.equals(user.email) && name.equals(user.name) && password.equals(user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, name, password);
+    }
+}
